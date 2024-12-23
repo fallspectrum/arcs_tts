d0ec668dd7f66da10873de0980dbcd327adde582
Travis Ladner <travis@tladner.com>
2024-12-22 19:14:06 -0800

    progress

--- src/BaseGame.lua
+++ src/BaseGame.lua
@@ -204,9 +204,9 @@ function BaseGame.components_visibility(params)
 end
 
 function BaseGame.setup(with_leaders, with_ll_expansion)
+    
+    BaseGame.destroy_grey_setup_menu_objects()
 
-    
-    
     local active_players = Global.call("getOrderedPlayers")
     Global.setVar("active_players", active_players)
     if (#active_players < 2 or #active_players > 4) then
